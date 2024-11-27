1498249cffc2b3351fb127ba8552b9766879a94a
yrachynska <yrachynska@kse.org.ua>
2024-11-27 14:50:33 +0200

    Change program

--- file.py
+++ file.py
@@ -4,16 +4,21 @@ import numpy as np
 def rivn1(a,b,c,x):
     return a*x**2+b*x+c
 
-a = int(input("Enter a: "))
-b = int(input("Enter b: "))
-c = int(input("Enter c: "))
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
 
 p = []
 
-x = np.arange(-100,100)
-y1 = rivn1(a,b,c,x)
-y2 = np.log2(x)
+x1 = np.arange(-100,100)
+x2 = np.arange(0.1,100)
+y1 = rivn1(a,b,c,x1)
+y2 = np.log2(x2)
 
-plt.plot(x,y1,x,y2)
-plt.show()
+for i in range(0,100):
+    if y1[i] == y2[i]:
+        p.append(i)
+
+plt.plot(x1,y1,x2,y2)
 print(p)
+plt.show()
